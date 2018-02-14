68713399e9d0cee0eb888b25454545b8c74233c3
Lwarren51 <36454305+Lwarren51@users.noreply.github.com>
2018-02-14 12:07:00 +0430

    Add files via upload

---
+++ P2T1_SalesPrediction_WarrenLorenzo.py
@@ -0,0 +1,20 @@
+# CTI-110
+# P2T1 - Sales Prediction
+# Lorenzo Warren
+# February 18, 2018
+#
+
+# Get Corporate 2017 Projected Total Sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit is $', format (profit, ',.2f'))
+
+
+
+
+
+
---
+++ P2T1_SalesPrediction_WarrenLorenzo.py shell.py
@@ -0,0 +1,24 @@
+Python 3.6.4 (v3.6.4:d48ecebad5, Dec 18 2017, 21:07:28) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: /Users/lorenzowarren/Desktop/CTI 110/P2T1_SalesPrediction_WarrenLorenzo.py 
+Enter the projected sales: 100
+The profit is $ 23.0
+>>> 
+ RESTART: /Users/lorenzowarren/Desktop/CTI 110/P2T1_SalesPrediction_WarrenLorenzo.py 
+Enter the projected sales: 1000
+The profit is $ 230.0
+>>> 
+ RESTART: /Users/lorenzowarren/Desktop/CTI 110/P2T1_SalesPrediction_WarrenLorenzo.py 
+Enter the projected sales: 10000
+The profit is $ 2300.0
+>>> 
+ RESTART: /Users/lorenzowarren/Desktop/CTI 110/P2T1_SalesPrediction_WarrenLorenzo.py 
+Enter the projected sales: 1000
+The profit is $ 230.00
+>>> 
+ RESTART: /Users/lorenzowarren/Desktop/CTI 110/P2T1_SalesPrediction_WarrenLorenzo.py 
+Enter the projected sales: 10000
+The profit is $ 2,300.00
+>>> 
