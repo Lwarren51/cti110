81be63c4067a3f5094fbfee7b66c33781b587dc6
Lwarren51 <36454305+Lwarren51@users.noreply.github.com>
2018-02-18 16:51:42 +0430

    Add files via upload

---
+++ P2HW1_DistanceTraveled_WarrenLorenzo.py
@@ -0,0 +1,26 @@
+# CTI-110
+# P2HW1 - Distance Traveled
+# Lorenzo Warren
+# February 18, 2018
+#
+
+# Estimate The Distance The Car Will Travel.
+Distance = ('Enter the projected speed: ')
+
+# Calculate the distanceAfter6 as 360 of the total speed.
+distance = 70 * 6
+
+# Display the distance.
+print('The distance is time', distance)
+
+# Calculate the distanceAfter10 as 360 of the total speed.
+distance = 70 * 10
+
+# Display the distance.
+print('The distance is time', distance)
+
+# Calculate the distanceAfter15 as 360 of the total speed.
+distance = 70 * 15
+
+# Display the distance.
+print('The distance is time', distance)
---
+++ P2HW1_DistanceTraveled_WarrenLorenzo_Shell.py
@@ -0,0 +1,9 @@
+Python 3.6.4 (v3.6.4:d48ecebad5, Dec 18 2017, 21:07:28) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: /Users/lorenzowarren/Desktop/CTI 110/Python Two Due 2-18/P2HW1_DistanceTraveled_WarrenLorenzo.py 
+The distance is time 420
+The distance is time 700
+The distance is time 1050
+>>> 
