d49850b43ccde1d154ce840e9317a0bf83b74ca2
Lwarren51 <36454305+Lwarren51@users.noreply.github.com>
2018-02-19 00:36:36 +0430

    Add files via upload

---
+++ P2HW2_TipTaxTotal_WarrenLorenzo.py
@@ -0,0 +1,43 @@
+# CTI-110
+# P2HW2 - Tip, Tax, and Total
+# Lorenzo Warren
+# February 18, 2018
+#
+
+# Get The Projected Total Food Cost.
+food_cost = float(input('Enter the food cost: '))
+
+# Calculate the total cost as tip at 18 percent of the food cost.
+tipAmount = food_cost * 0.18
+
+# Display the totalCost.
+print('The tipAmount is $', format (tipAmount, ',.2f'))
+
+
+
+
+# Get The Projected Total Food Cost.
+food_cost = float(input('Enter the food cost: '))
+
+# Calculate the total cost as 7 percent sales tax.
+salesTax = food_cost * 0.07
+
+# Display the totalCost.
+print('The salesTax is $', format (salesTax, ',.2f'))
+
+
+
+
+
+# Get The Total Food Cost Of The Meal.
+total_cost = ('Enter the food cost: ')
+
+# Calculate the total cost of the tip, tax and meal.
+totalCost = 9 + 3.50 + 50
+
+# Display the totalCost.
+print('The totalCost of the meal is $', format (totalCost, ',.2f'))
+
+
+
+
---
+++ P2HW2_TipTaxTotal_WarrenLorenzo_shell.py
@@ -0,0 +1,11 @@
+Python 3.6.4 (v3.6.4:d48ecebad5, Dec 18 2017, 21:07:28) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: /Users/lorenzowarren/Desktop/CTI 110/Python Two Due 2-18/P2HW2_TipTaxTotal_WarrenLorenzo.py 
+Enter the food cost: 50
+The tipAmount is $ 9.00
+Enter the food cost: 50
+The salesTax is $ 3.50
+The totalCost of the meal is $ 62.50
+>>> 
