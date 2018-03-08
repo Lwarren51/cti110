4ce769b6586dc0e891771f823b08ef13fbc928fd
Lwarren51 <36454305+Lwarren51@users.noreply.github.com>
2018-03-08 16:33:38 +0430

    Add files via upload

---
+++ P3T1_AreaOfRectangle_WarrenLorenzo_SHELL_ifelse.py
@@ -0,0 +1,25 @@
+Python 3.6.4 (v3.6.4:d48ecebad5, Dec 18 2017, 21:07:28) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: /Users/lorenzowarren/Desktop/CTI 110/IF-ELSE .py =========
+Enter the length of rectangle 1: 25
+Enter the width of rectangle 1: 50
+Enter the length of rectangle 2: 40
+Enter the width of rectangle 2: 80
+Rectangle 2 has the greater area.
+>>> 
+========= RESTART: /Users/lorenzowarren/Desktop/CTI 110/IF-ELSE .py =========
+Enter the length of rectangle 1: 50
+Enter the width of rectangle 1: 100
+Enter the length of rectangle 2: 45
+Enter the width of rectangle 2: 90
+Rectangle 1 has the greater area.
+>>> 
+========= RESTART: /Users/lorenzowarren/Desktop/CTI 110/IF-ELSE .py =========
+Enter the length of rectangle 1: 25
+Enter the width of rectangle 1: 50
+Enter the length of rectangle 2: 25
+Enter the width of rectangle 2: 50
+Both have the same area.
+>>> 
