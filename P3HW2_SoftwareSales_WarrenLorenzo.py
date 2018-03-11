4c3c6322a558ecb89473a819b606986682ffed66
Lwarren51 <36454305+Lwarren51@users.noreply.github.com>
2018-03-11 16:56:02 +0430

    Add files via upload

---
+++ P3HW2_SoftwareSales_WarrenLorenzo.py
@@ -0,0 +1,71 @@
+# CTI-110
+# P3HW2 - Software Sales
+# Lorenzo Warren
+# March 11, 2018
+
+# Get the quantity of the packages purchased 1.
+quantity10_19 = float(input('Enter the number of packages purchased 1: '))
+
+# Calculate the amount the discount total purchased.
+discount = quantity10_19 * 99
+
+# Display the discount.
+print('The discount is $', format (discount, ',.2f'))
+
+# Calculate the amount the discount total purchased.
+totalCost = 99 - quantity10_19 * 99
+
+# Display the totalCost.
+print('The totalCost is $', format (totalCost, ',.2f'))
+
+
+
+
+# Get the quantity of the packages purchased 2.
+quantity20_49 = float(input('Enter the number of packages purchased 2: '))
+
+# Calculate the amount the discount total purchased.
+discount = quantity20_49 * 99
+
+# Display the discount.
+print('The discount is $', format (discount, ',.2f'))
+
+# Calculate the amount the discount total purchased.
+totalCost = 99 - quantity20_49 * 99
+
+# Display the totalCost.
+print('The totalCost is $', format (totalCost, ',.2f'))
+
+
+
+# Get the quantity of the packages purchased 3.
+quantity50_99 = float(input('Enter the number of packages purchased 3: '))
+
+# Calculate the amount the discount total purchased.
+discount = quantity50_99 * 99
+
+# Display the discount.
+print('The discount is $', format (discount, ',.2f'))
+
+# Calculate the amount the discount total purchased.
+totalCost = 99 - quantity50_99 * 99
+
+# Display the totalCost.
+print('The totalCost is $', format (totalCost, ',.2f'))
+
+
+
+# Get the quantity of the packages purchased 4.
+quantity100 = float(input('Enter the number of packages purchased 4: '))
+
+# Calculate the amount the discount total purchased.
+discount = quantity100 * 99
+
+# Display the discount.
+print('The discount is $', format (discount, ',.2f'))
+
+# Calculate the amount the discount total purchased.
+totalCost = 99 - quantity100 * 99
+
+# Display the totalCost.
+print('The totalCost is $', format (totalCost, ',.2f'))
---
+++ P3HW2_SoftwareSales_WarrenLorenzo_SHELL.py
@@ -0,0 +1,18 @@
+Python 3.6.4 (v3.6.4:d48ecebad5, Dec 18 2017, 21:07:28) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: /Users/lorenzowarren/Desktop/10.py ================
+Enter the number of packages purchased 1: 0.10
+The discount is $ 9.90
+The totalCost is $ 89.10
+Enter the number of packages purchased 2: 0.20
+The discount is $ 19.80
+The totalCost is $ 79.20
+Enter the number of packages purchased 3: 0.30
+The discount is $ 29.70
+The totalCost is $ 69.30
+Enter the number of packages purchased 4: 0.40
+The discount is $ 39.60
+The totalCost is $ 59.40
+>>> 
