eb9f2a401f2e2d7035efc949581356426f0e103e
Lwarren51 <36454305+Lwarren51@users.noreply.github.com>
2018-04-22 20:31:26 +0430

    Add files via upload

---
+++ P5T2_FeetToInches_LorenzoWarren.py
@@ -0,0 +1,24 @@
+# A brief description of the project
+# April 22, 2018
+# CTI-110 P5T2_FeetToInches 
+# Lorenzo Warren
+#
+
+# Constant for the number of inches per foot.
+INCHES_PER_FOOT = 12
+
+# main function
+def main():
+    # Get a number of feet from the user.
+    feet = int(input('Enter a number of feet: '))
+
+    # Convert that to inches.
+    print(feet, 'equals', feet_to_inches(feet), 'inches.')
+
+
+# The feet_to_inches functions converts feet to inches.
+def feet_to_inches(feet):
+    return feet * INCHES_PER_FOOT
+
+# Call the main function.
+main()
